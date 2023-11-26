4e6c2206705bf8abf9b8538765651948f7281fb4
Konstantin <konstantinkostenyuk@gmail.com>
2023-11-26 13:11:34 +0100

    26.11.23 commit

--- src/main/java/com/example/myreportfromcsv/MyReportFromCsvApplication.java
+++ src/main/java/com/example/myreportfromcsv/MyReportFromCsvApplication.java
@@ -4,15 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //todo export to postgresSQL to make a reserve copy
+
 //todo check every data if it was changed and make changes in the DB
-//todo make output for preparing Acts: connect postgres
 
 
 @SpringBootApplication
 public class MyReportFromCsvApplication {
 
+
+
     public static void main(String[] args) {
         SpringApplication.run(MyReportFromCsvApplication.class, args);
-
+        System.out.println();
     }
 }
